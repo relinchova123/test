c2d1b6d868d654cd53667e8bb698ca8f13ba839d
relinchova123 <relinchova@pmg-pernik.com>
2025-10-22 17:09:18 +0300

    initial

---
+++ Calculator2025/Calculator/Calculator/OptionsManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "ex - Exit",
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+             };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine( $"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine( $"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine( $"{a} - {b} = {a - b}");
+        }
+
+    }
+}
---
+++ Calculator2025/Calculator/Calculator/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace Calculator
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Console Calculator App");
+                Console.WriteLine(new string('-', 15));
+
+                Console.Write("a = ");
+                double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+                Console.Write("b = ");
+                double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+                Console.WriteLine("Choose one from the listed options:");
+                foreach (string option in OptionsManager.OptionsList)
+                {
+                    Console.WriteLine($"\t{option}");
+                }
+                try
+                {
+                    Console.Write("Option: ");
+                    string choice = Console.ReadLine();
+
+                    if (choice == "ex")
+                    {
+                        break;
+                    }
+                    switch (choice)
+                    {
+                        case "a":
+                            OptionsManager.Add(a, b);
+                            break;
+                        case "s":
+                           OptionsManager.Subtract(a, b);
+                            break;
+                        case "m":
+                           OptionsManager.Multiply(a, b);
+                            break;
+                    }
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
